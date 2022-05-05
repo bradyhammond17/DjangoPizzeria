c0260d43dbbaf5d98cdfc6b2562ee59999ce34e0
Brady Hammond <brady_hammond1.baylor.edu>
2022-05-05 10:39:55 -0500

    worked on topics, next figure out url

--- pizzas/views.py
+++ pizzas/views.py
@@ -13,3 +13,11 @@ def pizzas(request):
 
     return render(request, 'pizzas/pizzas.html', context)
 
+def pizza(request, pizza_id):
+    ## view must be consistent with url
+    pizza = Pizza.objects.get(id=pizza_id)
+    entries = pizza.toppings_set.order_by('-topping_name')
+
+    context = {'pizza':pizza,'entries':entries}
+
+    return render(request, 'pizzas/pizza.html', context)
