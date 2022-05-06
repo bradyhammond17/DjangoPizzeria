2315e190473e399ca518d3efc15ccf63f75798a9
Brady Hammond <brady_hammond1.baylor.edu>
2022-05-06 12:14:28 -0500

    did styling

--- Pizzeria/settings.py
+++ Pizzeria/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Settings for django-bootstrap4
+BOOTSTRAP4 = {
+    'include_jquery' : True
+}
